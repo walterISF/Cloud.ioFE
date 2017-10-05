62417c889455b87fc5eae5e6f3afa99c07f2c1c9
Walter Inacio <wisf_94@hotmail.com>
2017-10-05 18:02:52 -0300

    template login criado

---
+++ app/controller/loginController.js
@@ -0,0 +1,6 @@
+app.controller('loginController', ['$scope', '$http', '$location', 'apiUrl', function($scope, $http, $location, apiUrl){
+    $scope.login = function( path )
+    {
+        $location.path(path);
+    }
+}]);

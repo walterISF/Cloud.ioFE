a7016228692a86e05153f0164a2008419c10b91c
Walter Inacio <wisf_94@hotmail.com>
2017-10-10 22:20:13 -0300

    ajustando responsivo e html Gerenciar Droplet

--- app/controller/cadastrarDropletController.js
+++ app/controller/cadastrarDropletController.js
@@ -1,48 +1,61 @@
 app.controller('cadastrarDropletController', ['$scope', '$http', 'apiUrl', function($scope, $http, apiUrl){
-    $scope.init = function(){
-    	$(function () {
-    $('.btn-radio').click(function(e) {
-        $('.btn-radio').not(this).removeClass('active')
-    		.siblings('input').prop('checked',false)
-            .siblings('.img-radio').css('opacity','0.5');
-    	$(this).addClass('active')
-            .siblings('input').prop('checked',true)
-    		.siblings('.img-radio').css('opacity','1');
-    });
-});
+    $scope.init = function()
+    {
+        $(function () 
+        {
+            $('.btn-radio').click(function(e) 
+            {
+                $('.btn-radio').not(this).removeClass('active')
+                    .siblings('input').prop('checked',false)
+                    .siblings('.img-radio').css('opacity','0.5');
 
-$(function () {
-    $('.btn-plan').click(function(e) {
-        $('.btn-plan').not(this).removeClass('active')
-    		.siblings('input').prop('checked',false)
-            .siblings('.img-plan').css('opacity','0.5');
-    	$(this).addClass('active')
-            .siblings('input').prop('checked',true)
-    		.siblings('.img-plan').css('opacity','1');
-    });
-});
+                $(this).addClass('active')
+                    .siblings('input').prop('checked',true)
+                    .siblings('.img-radio').css('opacity','1');
+            });
+        });
 
-$(function () {
-    $('.btn-services').click(function(e) {
-        $('.btn-services').not(this).removeClass('active')
-    		.siblings('input').prop('checked',false)
-            .siblings('.img-services').css('opacity','0.5');
-    	$(this).addClass('active')
-            .siblings('input').prop('checked',true)
-    		.siblings('.img-services').css('opacity','1');
-    });
-});
+        $(function () 
+        {
+            $('.btn-plan').click(function(e) 
+            {
+                $('.btn-plan').not(this).removeClass('active')
+                    .siblings('input').prop('checked',false)
+                    .siblings('.img-plan').css('opacity','0.5');
 
-$(function () {
-    $('.btn-os').click(function(e) {
-        $('.btn-os').not(this).removeClass('active')
-            .siblings('input').prop('checked',false)
-            .siblings('.img-os').css('opacity','0.5');
-        $(this).addClass('active')
-            .siblings('input').prop('checked',true)
-            .siblings('.img-os').css('opacity','1');
-    });
-});
+                $(this).addClass('active')
+                    .siblings('input').prop('checked',true)
+                    .siblings('.img-plan').css('opacity','1');
+            });
+        });
+
+        $(function () 
+        {
+            $('.btn-services').click(function(e) 
+            {
+                $('.btn-services').not(this).removeClass('active')
+                    .siblings('input').prop('checked',false)
+                    .siblings('.img-services').css('opacity','0.5');
+
+                $(this).addClass('active')
+                    .siblings('input').prop('checked',true)
+                    .siblings('.img-services').css('opacity','1');
+            });
+        });
+
+        $(function () 
+        {
+            $('.btn-os').click(function(e) 
+            {
+                $('.btn-os').not(this).removeClass('active')
+                    .siblings('input').prop('checked',false)
+                    .siblings('.img-os').css('opacity','0.5');
+                    
+                $(this).addClass('active')
+                    .siblings('input').prop('checked',true)
+                    .siblings('.img-os').css('opacity','1');
+            });
+        });
     }
 }]);
 
--- app/controller/gerenciarDropletsController.js
+++ app/controller/gerenciarDropletsController.js
@@ -1,3 +1,20 @@
 app.controller('gerenciarDropletsController', ['$scope', '$http', 'apiUrl', function($scope, $http, apiUrl){
-    
+
+    $scope.droplets = 
+    [
+        {
+            title: 'Batata',
+            description: 'Pao de Batata',
+            dataCriacao: new Date('2013' , '08', '12'),
+            dataVencimento: new Date('2013' , '12', '08')
+        },
+        {
+            title: 'Batata2',
+            description: 'Pao de Batata2',
+            dataCriacao: new Date('2013' , '08', '12'),
+            dataVencimento: new Date('2013' , '12', '08')
+        }
+            
+    ]
+
 }]);
